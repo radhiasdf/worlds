f66b87598691358e91762ca42ff7fe4355bd962b
radhiasdf <radhityarr@gmail.com>
2022-12-25 13:09:04 +1300

    moved setting window and stuff

--- main.py
+++ main.py
@@ -4,28 +4,23 @@ from player import Player
 from world import World
 import shelve
 import os
+from constants import FPS
 user_data = shelve.open("user_data")
-pg.init()
-pg.display.set_caption('Twuga')
-pg.display.set_icon(pg.image.load(os.path.join("assets", "grass3.png")))
-win = pg.display.set_mode((pg.display.Info().current_w*.7, pg.display.Info().current_h*.7))
 
-bookImg = pg.image.load(os.path.join("assets", "pain in the ass book.png"))
-playButtonImg = pg.image.load(os.path.join("assets", "play.png"))
-guiImgs = [bookImg, playButtonImg]
 
-for i, img in enumerate(guiImgs):
-    guiImgs[i] = pg.transform.scale(img, (img.get_width()*3, img.get_height()*3))
-
-class Game:
+class GameManager:
     def __init__(self):
+        pg.init()
+        pg.display.set_caption('Twuga')
+        pg.display.set_icon(pg.image.load(os.path.join("assets", "grass3.png")))
+        self.win = pg.display.set_mode((pg.display.Info().current_w*.7, pg.display.Info().current_h*.7))
 
         self.state_stack = [MainMenu(self)]
 
         run = True
         prev_time = time.time()
         while run:
-            pg.time.Clock().tick(60)
+            pg.time.Clock().tick(FPS)
             dt = time.time() - prev_time  # delta time
             prev_time = time.time()
 
@@ -35,6 +30,7 @@ class Game:
                     run = False
 
             self.state_stack[-1].update(dt, events)
+            self.state_stack[-1].draw(self.win)
 
         try:
             user_data['world'] = self.state_stack[-1].world.mapData
@@ -47,40 +43,45 @@ class Game:
 class MainMenu:
     def __init__(self, game):
         self.game = game
-        pass
+
+        bookImg = pg.image.load(os.path.join("assets", "pain in the ass book.png"))
+        playButtonImg = pg.image.load(os.path.join("assets", "play.png"))
+        self.guiImgs = [bookImg, playButtonImg]
+        for i, img in enumerate(self.guiImgs):
+            self.guiImgs[i] = pg.transform.scale(img, (img.get_width() * 3, img.get_height() * 3))
 
     def update(self, dt, events):
-        print(events)
 
         for e in events:
             if e.type == pg.KEYDOWN and e.key == pg.K_SPACE:
                 self.game.state_stack.append(InGame(self.game, World(user_data['world'])))
 
+    def draw(self, win):
         win.fill('white')
         for i in range(0, 2):
-            win.blit(guiImgs[i], ((win.get_width() - guiImgs[i].get_width())/2, (win.get_height() - guiImgs[i].get_height())/2))
+            win.blit(self.guiImgs[i], ((win.get_width() - self.guiImgs[i].get_width())/2, (win.get_height() - self.guiImgs[i].get_height())/2))
         pg.display.update()
 
 
 class InGame:
     def __init__(self, game, world_data):
         self.game = game
-        self.player = Player((win.get_width()/2, win.get_height()/2))
+        self.player = Player((game.win.get_width()/2, game.win.get_height()/2), self.game)
         self.world = world_data
 
         self.cameraLocation = pg.Vector2(0, 0)  # is in pixels
 
     def update(self, dt, events):
-        print('in game')
         for e in events:
             if e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
                 self.game.state_stack.append(PauseMenu(self.game))
 
         self.cameraLocation = self.player.update(dt, self.cameraLocation, events)
 
+    def draw(self, win):
         win.fill('darkolivegreen3')
         self.world.draw(win, self.cameraLocation)
-        self.player.draw(win, dt)
+        self.player.draw(win)
         pg.display.update()
 
 
@@ -90,9 +91,13 @@ class PauseMenu:
         pass
 
     def update(self, dt, events):
-        print('in pause menu')
         for e in events:
             if e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
                 self.game.state_stack.pop()
 
-game = Game()
+    def draw(self, win):
+        win.fill('yellow')
+        pg.display.update()
+
+
+game = GameManager()
--- player.py
+++ player.py
@@ -3,10 +3,13 @@ import os
 import shadows
 from constants import TILE_ZOOM
 sprite_size = TILE_ZOOM*2
+from solitaire.solitaire import InSolitaire
 
 
 class Player:
-    def __init__(self, pos):
+    def __init__(self, pos, game):
+        self.game = game  # is putting the game instance in a bunch of other instances bad
+
         self.imgsWalkingFront = [pg.image.load(os.path.join("assets", "pixil-gif-drawing-tamarin-20221217", f"sprite_{i}.png")) for i in range(4)]
         for i, img in enumerate(self.imgsWalkingFront):
             self.imgsWalkingFront[i] = pg.transform.scale(img, (sprite_size, sprite_size)).convert_alpha()
@@ -17,10 +20,11 @@ class Player:
         self.speed = 150
         self.animationSpeed = 7
         self.frameIndex = 0
-        self.count = 0
+        self.img = self.currentAnimationImgs[int(self.frameIndex)]
 
     def input(self, events):
         keys = pg.key.get_pressed()  # does this slow down code
+        print(len(keys))
         if keys[pg.K_UP] or keys[pg.K_w]:
             self.direction.y = -1
         elif keys[pg.K_DOWN] or keys[pg.K_s]:
@@ -35,17 +39,28 @@ class Player:
         else:
             self.direction.x = 0
 
+        for e in events:
+            if e.type == pg.KEYDOWN:
+                if e.key == pg.K_SPACE:
+                    self.game.state_stack.append(InSolitaire(game_manager=self.game))
+
+                """if e.key in (pg.K_UP,pg.K_w):
+                    """
+
     def update(self, dt, camera_location, events):
         self.input(events)
         camera_location += self.direction * int(self.speed * dt) # there was a bug where going down and right were slower than up and left; i suspect the rounding to rect.center was floor, causing -1.7 --> 2 and 1.7 --> 1
-        return camera_location
 
-    def draw(self, win, dt):
         if self.direction != (0, 0):
             self.frameIndex += self.animationSpeed * dt
         if self.frameIndex > len(self.currentAnimationImgs):
             self.frameIndex = 0
-        img = self.currentAnimationImgs[int(self.frameIndex)]
-        shadows.draw_shadow(img, win, self.rect.topleft, self.rect.bottom-6)
-        win.blit(img, (self.rect.x, self.rect.y))
+        self.img = self.currentAnimationImgs[int(self.frameIndex)]
+
+        return camera_location
+
+    def draw(self, win):
+
+        shadows.draw_shadow(self.img, win, self.rect.topleft, self.rect.bottom-6)
+        win.blit(self.img, (self.rect.x, self.rect.y))
 
